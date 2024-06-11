c6d299d1435db7d0a0e6149fa44e1ed691898b55
Jan Engelhardt <jengelh@inai.de>
2024-06-11 15:54:22 +0200

    mapi_lib: fix out-of-bounds access in PROBLEM_ARRAY::transform
    ==93973==ERROR: AddressSanitizer: heap-buffer-overflow on address
    0x502000087734 at pc 0x7ff932e5635f bp 0x7ff8a79fd240 sp
    0x7ff8a79fd238
    READ of size 2 at 0x502000087734 thread T73
        f0 PROBLEM_ARRAY::transform(unsigned short const*) lib/mapi/element_data.cpp:407
        f1 folder_object::set_properties(TPROPVAL_ARRAY const*, PROBLEM_ARRAY*) exch/emsmdb/folder_object.cpp:522
        f2 rop_setproperties(TPROPVAL_ARRAY const*, PROBLEM_ARRAY*, LOGMAP*, unsigned char, unsigned int) exch/emsmdb/oxcprpt.cpp:385
        f3 rop_dispatch(rop_request const&, rop_response*&, unsigned int*, unsigned char) exch/emsmdb/rop_dispatch.cpp:1062
    
    0x502000087734 is located 2 bytes after 2-byte region [0x502000087730,0x502000087732)
    allocated by thread T73 here:
        f0 operator new[](unsigned long) (/lib64/libasan.so.8)
        f1 std::__detail::_MakeUniq<char []>::__array std::make_unique<char []>(unsigned long) /usr/include/c++/13/bits/unique_ptr.h:1085
        f2 alloc_context::alloc(unsigned long) include/gromox/util.hpp:39
        f3 pdu_processor_ndr_stack_alloc(int, unsigned long) exch/http/pdu_processor.cpp:162
        f4 emsmdb::common_util_alloc(unsigned long) exch/emsmdb/common_util.cpp:88
        f5 unsigned short* emsmdb::cu_alloc<unsigned short>(unsigned long) exch/emsmdb/common_util.hpp:37
        f6 folder_object::set_properties(TPROPVAL_ARRAY const*, PROBLEM_ARRAY*) exch/emsmdb/folder_object.cpp:482
        f7 rop_setproperties(TPROPVAL_ARRAY const*, PROBLEM_ARRAY*, LOGMAP*, unsigned char, unsigned int) exch/emsmdb/oxcprpt.cpp:385
        f8 rop_dispatch(rop_request const&, rop_response*&, unsigned int*, unsigned char) exch/emsmdb/rop_dispatch.cpp:1062
    
    According to this report, poriginal_indices must have been a
    1-element array and tmp_propvals 5-element, of which the 3rd,
    PR_CHANGE_KEY, (somewhat mysteriously) was reported in PROBLEM_ARRAY.
    
    Fixes: gromox-0~666
    References: GXL-503

--- exch/zcore/message_object.cpp
+++ exch/zcore/message_object.cpp
@@ -7,6 +7,7 @@
 #include <cstring>
 #include <memory>
 #include <utility>
+#include <vector>
 #include <libHX/string.h>
 #include <gromox/defs.h>
 #include <gromox/ext_buffer.hpp>
@@ -813,13 +814,12 @@ BOOL message_object::get_properties(const PROPTAG_ARRAY *pproptags,
 }
 
 static BOOL message_object_set_properties_internal(message_object *pmessage,
-    BOOL b_check, const TPROPVAL_ARRAY *ppropvals)
+    BOOL b_check, const TPROPVAL_ARRAY *ppropvals) try
 {
 	uint8_t tmp_bytes[3];
 	PROBLEM_ARRAY problems;
 	PROBLEM_ARRAY tmp_problems;
 	TPROPVAL_ARRAY tmp_propvals;
-	uint16_t *poriginal_indices;
 	TPROPVAL_ARRAY tmp_propvals1;
 	TAGGED_PROPVAL propval_buff[3];
 	
@@ -833,9 +833,7 @@ static BOOL message_object_set_properties_internal(message_object *pmessage,
 	tmp_propvals.ppropval = cu_alloc<TAGGED_PROPVAL>(ppropvals->count);
 	if (tmp_propvals.ppropval == nullptr)
 		return FALSE;
-	poriginal_indices = cu_alloc<uint16_t>(ppropvals->count);
-	if (poriginal_indices == nullptr)
-		return FALSE;
+	std::vector<uint16_t> poriginal_indices;
 	for (unsigned int i = 0; i < ppropvals->count; ++i) {
 		const auto &pv = ppropvals->ppropval[i];
 		if (b_check) {
@@ -873,7 +871,7 @@ static BOOL message_object_set_properties_internal(message_object *pmessage,
 			}
 		}
 		tmp_propvals.ppropval[tmp_propvals.count] = pv;
-		poriginal_indices[tmp_propvals.count++] = i;
+		poriginal_indices.push_back(i);
 	}
 	if (tmp_propvals.count == 0)
 		return TRUE;
@@ -897,6 +895,9 @@ static BOOL message_object_set_properties_internal(message_object *pmessage,
 			return FALSE;	
 	}
 	return TRUE;
+} catch (const std::bad_alloc &) {
+	mlog(LV_ERR, "E-1748: ENOMEM");
+	return false;
 }
 
 BOOL message_object::set_properties(TPROPVAL_ARRAY *ppropvals)
@@ -906,13 +907,12 @@ BOOL message_object::set_properties(TPROPVAL_ARRAY *ppropvals)
 						pmessage, TRUE, ppropvals);
 }
 
-BOOL message_object::remove_properties(const PROPTAG_ARRAY *pproptags)
+BOOL message_object::remove_properties(const PROPTAG_ARRAY *pproptags) try
 {
 	auto pmessage = this;
 	PROBLEM_ARRAY problems;
 	PROBLEM_ARRAY tmp_problems;
 	PROPTAG_ARRAY tmp_proptags;
-	uint16_t *poriginal_indices;
 	
 	if (!pmessage->b_writable)
 		return FALSE;
@@ -924,9 +924,7 @@ BOOL message_object::remove_properties(const PROPTAG_ARRAY *pproptags)
 	tmp_proptags.pproptag = cu_alloc<uint32_t>(pproptags->count);
 	if (tmp_proptags.pproptag == nullptr)
 		return FALSE;
-	poriginal_indices = cu_alloc<uint16_t>(pproptags->count);
-	if (poriginal_indices == nullptr)
-		return FALSE;
+	std::vector<uint16_t> poriginal_indices;
 	for (unsigned int i = 0; i < pproptags->count; ++i) {
 		const auto tag = pproptags->pproptag[i];
 		if (msgo_is_readonly_prop(pmessage, tag)) {
@@ -934,7 +932,7 @@ BOOL message_object::remove_properties(const PROPTAG_ARRAY *pproptags)
 			continue;
 		}
 		tmp_proptags.pproptag[tmp_proptags.count] = tag;
-		poriginal_indices[tmp_proptags.count++] = i;
+		poriginal_indices.push_back(i);
 	}
 	if (tmp_proptags.count == 0)
 		return TRUE;
@@ -958,6 +956,9 @@ BOOL message_object::remove_properties(const PROPTAG_ARRAY *pproptags)
 			return FALSE;	
 	}
 	return TRUE;
+} catch (const std::bad_alloc &) {
+	mlog(LV_ERR, "E-1749: ENOMEM");
+	return false;
 }
 
 BOOL message_object::copy_to(message_object *pmessage_src,
